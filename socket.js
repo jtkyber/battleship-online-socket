48cb57c39dc33e356dd627731a9a1453f06ca7e1
jtkyber <43868043+jtkyber@users.noreply.github.com>
2021-05-09 20:02:57 -0600

    changed origin

--- socket.js
+++ socket.js
@@ -6,7 +6,7 @@ const server = http.createServer(app);
 const { Server } = require('socket.io');
 const io = new Server(server, {
     cors: {
-        origin: 'https://jtkyber.github.io/battleship-online/',
+        origin: '*',
         methods: ["GET", "POST", "PUT"]
     }
 });
