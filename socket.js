6fdb6c37e519235e76fa4786726642e428ec9a3f
jtkyber <43868043+jtkyber@users.noreply.github.com>
2021-05-09 19:26:08 -0600

    changed origin

--- socket.js
+++ socket.js
@@ -5,7 +5,7 @@ const server = http.createServer(app);
 const { Server } = require('socket.io');
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:3000',
+        origin: 'https://jtkyber.github.io/battleship-online/',
         methods: ["GET", "POST"]
     }
 });
@@ -45,6 +45,6 @@ io.on('connection', (socket) => {
 });
 
 
-server.listen(3001, () => {
-    console.log('listening on port 3001');
+server.listen(process.env.PORT, () => {
+    console.log(`listening on port ${process.env.PORT}`);
 });
