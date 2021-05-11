fee84956a66e2ae8f767a435c374fae1ea97db91
jtkyber <43868043+jtkyber@users.noreply.github.com>
2021-05-10 20:20:46 -0600

    fix

--- socket.js
+++ socket.js
@@ -8,8 +8,8 @@ const { Server } = require('socket.io');
 const io = new Server(server, {
     cors: {
         origin:
-        'http://localhost:3000',
-        // 'https://jtkyber.github.io/battleship-online',
+        // 'http://localhost:3000',
+        'https://jtkyber.github.io/battleship-online',
         methods: ["GET", "POST"]
     }
 });
