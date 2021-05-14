5e599046a29b8e00b12536a8cbe825dfe99dc2da
jtkyber <43868043+jtkyber@users.noreply.github.com>
2021-05-14 11:10:00 -0600

    switching origins

--- socket.js
+++ socket.js
@@ -7,8 +7,8 @@ const { Server } = require('socket.io');
 const io = new Server(server, {
     cors: {
         origin:
-        'http://localhost:3000',
-        // 'https://jtkyber.github.io',
+        // 'http://localhost:3000',
+        'https://jtkyber.github.io',
         methods: ["GET", "POST"]
     }
 });
