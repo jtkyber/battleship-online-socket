116bc6a87853a989a80d7e69ba997a3f39b23ed0
jtkyber <43868043+jtkyber@users.noreply.github.com>
2023-02-09 17:12:53 -0500

    fix

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ const { Server } = require('socket.io');
 
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:3000',
-        // '*',
+        origin: '*',
         methods: ["GET", "POST", "PUT"]
     }
 });
